d618214ffa7c49eb40cf4db9979f6cbbd0eeb41b
bochelly <antoine.borrelly@isep.fr>
2019-10-23 10:13:47 +0200

    Transformation des fichiers en PHP, séparation du menu latéral

--- admin/index.php
+++ admin/index.php
@@ -17,17 +17,9 @@
 	<header><h1>AUDASANTÉ</h1></header>
 
 
-	<nav> 
-		<ul>
-		    <a href="Accueil.html"><li  class='en_cours'><i class="fas fa-home fa-lg fa-fw" style="color:#A3A0FB;"><!--Icon--></i><span>Accueil</span></li></a>
-		    <a href="Dashboard.html"><li><i class="far fa-chart-bar fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Dashboard</span></li></a>
-		    <a href="Messages.html"><li><i class="fas fa-envelope fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Messages</span></li></a>	
-		    <a href="Patients.html"><li><i class="fas fa-users fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Patients</span></li></a>
-		    <a href="Calendrier.html"><li><i class="fas fa-calendar-alt fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Calendrier</span></li></a>	
-		    <a href="Assistance.html"><li><i class="far fa-question-circle fa-lg fa-fw" style="color:#A5A4BF;"></i><span>Assistance</span></li></a>	
-		    <a href="Parametres.html"><li><i class="fas fa-cogs fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Parametres</span></li></a>
-		</ul>
-	</nav>
+	<?php
+		require('menu_lateral_admin.php');
+	?>
 
 	<div id='top-bar'>
 		<ul>
--- admin/menu_lateral_admin.php
+++ admin/menu_lateral_admin.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-
-
-<html>
-
-
-<head>
-	<meta charset="utf-8" />
-	<link rel="stylesheet" href="../CSS/style.css" />
-	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'> <!--Permet de telecharger la police Source sans pro -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous"> <!-- Permet d'utiliser les émoticions de fontawesome -->
-	<title>Audasanté</title>
-</head>
-
-
-<body>
-	<header><h1>AUDASANTÉ</h1></header>
-
-
-	<nav> 
+<nav> 
 		<ul>
 		    <a href="Accueil.html"><li  class='en_cours'><i class="fas fa-home fa-lg fa-fw" style="color:#A3A0FB;"><!--Icon--></i><span>Accueil</span></li></a>
 		    <a href="Dashboard.html"><li><i class="far fa-chart-bar fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Dashboard</span></li></a>
@@ -28,6 +9,3 @@
 		    <a href="Parametres.html"><li><i class="fas fa-cogs fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Parametres</span></li></a>
 		</ul>
 	</nav>
-	
-	</body>
-</html>
