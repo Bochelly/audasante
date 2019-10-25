a537e18d3dde68c6fe7f67fb4b3ad6647488b742
Th0masso <55784802+Th0masso@users.noreply.github.com>
2019-10-25 04:43:03 +0200

    CSS page de connexion + responsive

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <head>
 	<meta charset=utf-8 /> 
 	<title>Connexion</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+	<link rel="stylesheet" href="css/style-connect.css" type="text/css" media="screen"/>
 	<link rel="icon" type="image/png" href="css/img/audalogo.png" />
 	
 	<style>
@@ -27,29 +27,32 @@
 
 <body>
 
-
-
-<img src="css/img/main_mesure_x2.png" style='position: fixed ;height: auto; width: 50%; left:0px; bottom:0px;'/>
-
+<div id=log-img></div>
 <div id=log-connect-container>
 
+	<div class='window'>
 
+		<div class='welcome' style="margin-bottom:1.5%;">AUDASANTÉ</div>
 
-	<div id=log-connect>
+		<div class='subtitle' style="margin-bottom:3%;">La musique au service de la vie</div>
+
 		<form method="get" action="connect.php">
-			<i class="" style="color:#43425D;"> </i><legend>A U D A S A N T É</legend>
-			<i class="" style="color:#43425D;"> </i><legend>E-mail</legend>
-			<input type="text" name="login"/>
-			<legend>Mot de passe</legend>
-			<input type="password" name="pswrd"/>
-			</br><input type="submit" name="submit" value="Connexion"/>
-			</br>Rester connecter <input type="radio" name="submit"/>
-		</form>
-	</div>
-	<div style="clear:both"></div>
-</div>
-</body>
+			<input type='email' placeholder='Email' class='input-line full-width'></input>
+			<input type='password' placeholder='Mot de passe' class='input-line full-width'></input>
 
+
+			<div class="switch">
+				<p>Se souvenir de moi</p>
+				<input type="checkbox" id="switch" name="submit"/><label for="switch"></label>
+			</div>
+
+			<button class="ghost-round full-width"  type="submit" name="submit" value="Connexion">Connexion</button>
+			<button class="ghost-round full-width"  type="submit" name="submit" value="Register">S'inscrire</button>
+		</form>
+
+	</div>
+</div>
+</body> 
 
 </html>
 
