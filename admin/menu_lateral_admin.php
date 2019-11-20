4263e673ae4b1855ebbe8897e4f1d16cebc1c9e5
Felixned <55989975+Felixned@users.noreply.github.com>
2019-11-20 11:33:29 +0100

    menu latéral dynamique (ajout couleur icon)

--- admin/menu_lateral_admin.php
+++ admin/menu_lateral_admin.php
@@ -1,12 +1,12 @@
 
 <nav> 
 		<ul>
-		    <a href="index"><li <?php if ($page_en_cours == 'accueil') {echo 'class="en_cours"';} ?>><i class="fas fa-home fa-lg fa-fw" style="color:#A3A0FB;"><!--Icon--></i><span>Accueil</span></li></a>
-		    <a href="dashboard"><li <?php if ($page_en_cours == 'dashboard') {echo 'class="en_cours"';} ?>><i class="far fa-chart-bar fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Dashboard</span></li></a>
-		    <a href="messagerie"><li <?php if ($page_en_cours == 'messagerie') {echo 'class="en_cours"';} ?>><i class="fas fa-envelope fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Messages</span></li></a>	
-		    <a href="Patients.html"><li <?php if ($page_en_cours == 'patients') {echo 'class="en_cours"';} ?>><i class="fas fa-users fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Patients</span></li></a>
-		    <a href="calendrier"><li <?php if ($page_en_cours == 'calendrier') {echo 'class="en_cours"';} ?>><i class="fas fa-calendar-alt fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Calendrier</span></li></a>	
-		    <a href="assistance"><li <?php if ($page_en_cours == 'assistance') {echo 'class="en_cours"';} ?>><i class="far fa-question-circle fa-lg fa-fw" style="color:#A5A4BF;"></i><span>Assistance</span></li></a>	
-		    <a href="parametre"><li <?php if ($page_en_cours == 'parametre') {echo 'class="en_cours"';} ?>><i class="fas fa-cogs fa-lg fa-fw" style="color:#A5A4BF;"> </i><span>Parametres</span></li></a>
+		    <a href="index"><li <?php if ($page_en_cours == 'accueil') {echo 'class="en_cours"';} ?>><i class="fas fa-home fa-lg fa-fw"><!--Icon--></i><span>Accueil</span></li></a>
+		    <a href="dashboard"><li <?php if ($page_en_cours == 'dashboard') {echo 'class="en_cours"';} ?>><i class="far fa-chart-bar fa-lg fa-fw"> </i><span>Dashboard</span></li></a>
+		    <a href="messagerie"><li <?php if ($page_en_cours == 'messagerie') {echo 'class="en_cours"';} ?>><i class="fas fa-envelope fa-lg fa-fw"> </i><span>Messages</span></li></a>	
+		    <a href="Patients.html"><li <?php if ($page_en_cours == 'patients') {echo 'class="en_cours"';} ?>><i class="fas fa-users fa-lg fa-fw"> </i><span>Patients</span></li></a>
+		    <a href="calendrier"><li <?php if ($page_en_cours == 'calendrier') {echo 'class="en_cours"';} ?>><i class="fas fa-calendar-alt fa-lg fa-fw"> </i><span>Calendrier</span></li></a>	
+		    <a href="assistance"><li <?php if ($page_en_cours == 'assistance') {echo 'class="en_cours"';} ?>><i class="far fa-question-circle fa-lg fa-fw"></i><span>Assistance</span></li></a>	
+		    <a href="parametre"><li <?php if ($page_en_cours == 'parametre') {echo 'class="en_cours"';} ?>><i class="fas fa-cogs fa-lg fa-fw"> </i><span>Parametres</span></li></a>
 		</ul>
 </nav>
