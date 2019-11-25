7a36c9f72e0e599662358421fbf3d51a2309d50e
Felixned <55989975+Felixned@users.noreply.github.com>
2019-11-25 13:48:53 +0100

    Revert "Revert "register""
    This reverts commit 16101080f1969a302c5a9627a31aba809e54c644.

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
 
 	<style>
 
-				.window form {
+		.window form {
 		  display: flex;
 		  flex-wrap:wrap;
 		  justify-content:space-evenly; 
@@ -16,17 +16,23 @@
 		  padding-top: 3% ;
 		  padding-left: 1% ;
 		  padding-right: 1% ;
-		  margin-left: 30% ;
-		  margin-right: 30% ;
+		  margin-left: 10% ;
+		  margin-right: 10% ;
 		  margin-bottom: 15% ; 
-		  height: 35%;
+		  height: 40%;
+		}
+
+		.input-line {
+  			height:20%;
+ 			width: 100%;
+ 			padding-bottom: 2%;
 		}
 
 		.input-line input:focus ~ .floating-label,
 		.input-line input:placeholder-shown ~ .floating-label,
 		.input-line input:not(:focus):valid ~ .floating-label{
 		  left: 0.1vw;
-		  top: -90%;
+		  top: -100%;
 		  font-size: 0.9vw;
 		}
 
@@ -34,6 +40,8 @@
 		{
 			text-align: center;
 		}
+
+		
 
 	</style>
 
@@ -47,51 +55,55 @@
 		<h1>Formulaire d'inscription</h1>
 		<form method="get" action="#">
 
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Nom</span>
+			<div class='gauche'>
+				<div class="input-line">
+					<input type='text' class='inputText' required></input>
+					Nom
+				</div>
+
+				<div class="input-line">
+					<input type='text' class='inputText' required></input>
+					Prénom
+				</div>
+
+				<div class="input-line">
+					<input type='text' class='inputText' required></input>
+					Numéro de sécurité sociale
+				</div>
+
+				<div class="input-line">
+					<input type='date' class='inputText' required></input>
+					Date de naissance
+				</div>
+
+				<div class="input-line" class="bouton_radio">
+					<INPUT type="radio" name="sex" value="H" checked> Homme <INPUT type="radio" name="sex" value="F"> Femme<br/>
+					Sexe
+				</div>
+
+				<div class="input-line">
+					<input type='text' class='inputText' required></input>
+					Hôpital
+				</div>
 			</div>
 
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Prénom</span>
-			</div>
 
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Date de naissance</span>
-			</div>
+			<div class='droite'>
+				<div class="input-line">
+					<input type='email' class='inputText' required></input>
+					E-mail
+				</div>
 
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Sexe</span>
-			</div>
+				<div class="input-line">
+					<input type="password" class="inputText" required/></input>
+					Mot de passe
+				</div>
 
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Numéro de sécurité sociale</span>
+				<div class="input-line">
+					<input type="password" class="inputText" required/></input>
+					Répéter mot de passe
+				</div>
 			</div>
-
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">E-mail</span>
-			</div>
-
-			<div class="input-line">
-				<input type="password" class="inputText" required/></input>
-				<span class="floating-label">Mot de passe</span>
-			</div>
-
-			<div class="input-line">
-				<input type="password" class="inputText" required/></input>
-				<span class="floating-label">Répéter mot de passe</span>
-			</div>
-
-			<div class="input-line">
-				<input type='text' class='inputText' required></input>
-				<span class="floating-label">Hôpital</span>
-			</div>
-
 			<button class="ghost-round bright"  type="submit" name="submit" value="Register" style="margin-top: 5%;">S'inscrire</button>
 		</form>
 	</div>
