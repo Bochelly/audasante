332a7ddcb12f71e5fd63f4c05723be9a745722c7
thomasso <t.hercule.sin@gmail.com>
2019-11-25 14:10:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 <head>
 	<meta charset=utf-8 /> 
 	<title>Connexion</title>
-	<link rel="stylesheet" href="css/style-connect.css" type="text/css" media="screen"/>
+	<link rel="stylesheet" href="css/style-connect.css" media="screen"/>
 	<link rel="icon" type="image/png" href="css/img/audalogo.png" />
 	
 	<style>
@@ -46,26 +46,24 @@
 		<form method="get" action="connect.php">
 
 			<div class="input-line">
-				<input type='text' class='inputText' id='login'required></input>
+				<input type='text' class='inputText' name='login'required></input>
 				<span class="floating-label">E-mail</span>
 			</div>
 
 			<div class="input-line">
-				<input type="password" class="inputText" id='pswrd'required/></input>
+				<input type="password" class="inputText" name='pswrd'required/></input>
 				<span class="floating-label">Mot de passe</span>
 			</div>
 
 			<div class="switch">
-				<input type="checkbox" id="switch" name="submit"/><label for="switch"></label>
+				<input type="checkbox" name="switch" name="submit"/><label for="switch"></label>
 				<p>Se souvenir de moi</p>
 			</div>
 
 			<a href="Patients.html"><span>Mot de passe oublié</span></a>
 
-			<span><?php echo $error_msg ?></span>
-
 			<button class="ghost-round dark"  type="submit" name="submit" value="Connexion">Connexion</button>
-			<button class="ghost-round bright"  onclick="self.location.href='register.php'" name="Register" >S'inscrire</button>
+			<a class="ghost-round bright"  onclick="self.location.href='register.php'">S'inscrire</a>
 		</form>
 
 	</div>
