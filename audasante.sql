79be97f2f12c9f8b224a50c3034c8dac29959b32
Felixned <55989975+Felixned@users.noreply.github.com>
2019-11-29 14:42:31 +0100

    ajout faq BDD

--- audasante.sql
+++ audasante.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 27 nov. 2019 à 20:54
+-- Généré le :  ven. 29 nov. 2019 à 13:39
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -51,7 +51,32 @@ CREATE TABLE IF NOT EXISTS `case` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `hospital`
+-- Structure de la table `faq`
+--
+
+DROP TABLE IF EXISTS `faq`;
+CREATE TABLE IF NOT EXISTS `faq` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(256) NOT NULL,
+  `answer` text NOT NULL,
+  `frequency` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `faq`
+--
+
+INSERT INTO `faq` (`id`, `subject`, `answer`, `frequency`) VALUES
+(1, 'Comment chercher un patient ?', 'Pour rechercher un patient, utiliser la barre de recherche à votre disposition en haut de l\'écran. Vous pouvez par exemple entrer le nom ou le prénom du patient recherché et il s\'affichera s\'il se trouve dans notre base de données. ', 0),
+(2, 'Comment ajouter un patient à la base de données ?', 'Dirigez-vous dans l\'onglet patients. Tous les patients qui sont liés à votre compte sont présents et vous pouvez utiliser le bouton \"ajouter un patient\" pour créer le compte d\'un patient et le lier automatiquement à votre compte.', 0),
+(3, 'Comment utiliser le calendrier Audasanté ?', 'C\'est très simple. Lier votre compte Audasanté avec votre compte Google afin que de lié le calendrier Audasanté à votre calendrier Google.', 0),
+(4, 'Que dois-je faire si ma question ne figure pas dans la FAQ ?', 'Si votre question question ne figure pas dans FAQ, vous pouvez nous contactez grâce aux moyens affichés dans l\'onglet \"Assistance\" du site Audasanté. Notre équipe est à votre écoute et vous répondra dans les plus brefs délais.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `patient`
 --
 
 DROP TABLE IF EXISTS `hospital`;
@@ -61,45 +86,6 @@ CREATE TABLE IF NOT EXISTS `hospital` (
   `adresse` varchar(64) NOT NULL,
   PRIMARY KEY (`id_hospital`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `medic`
---
-
-DROP TABLE IF EXISTS `medic`;
-CREATE TABLE IF NOT EXISTS `medic` (
-  `n_secu` bigint(20) NOT NULL,
-  `first_name` varchar(63) NOT NULL,
-  `last_name` varchar(63) NOT NULL,
-  `birth_date` date NOT NULL,
-  `work_place` varchar(63) NOT NULL,
-  `e_mail` varchar(63) NOT NULL,
-  `password` varchar(63) NOT NULL,
-  `last_connect` date NOT NULL,
-  `nb_try` int(16) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`n_secu`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `patient`
---
-
-DROP TABLE IF EXISTS `patient`;
-CREATE TABLE IF NOT EXISTS `patient` (
-  `n_secu` bigint(20) NOT NULL,
-  `first_name` varchar(63) CHARACTER SET utf8 NOT NULL,
-  `last_name` varchar(63) CHARACTER SET utf8 NOT NULL,
-  `birth_date` date NOT NULL,
-  `e_mail` varchar(63) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(63) CHARACTER SET utf8 NOT NULL,
-  `last_connect` date NOT NULL,
-  `nb_try` int(16) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`n_secu`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -124,12 +110,12 @@ CREATE TABLE IF NOT EXISTS `reflex` (
 
 DROP TABLE IF EXISTS `rhythm`;
 CREATE TABLE IF NOT EXISTS `rhythm` (
-  `n_test` int(255) NOT NULL,
+  `id_test` int(255) NOT NULL,
   `shift_avg` int(12) NOT NULL,
   `shift_min` int(12) NOT NULL,
   `shift_max` int(12) NOT NULL,
-  PRIMARY KEY (`n_test`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_test`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -163,27 +149,20 @@ CREATE TABLE IF NOT EXISTS `test` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `n_secu` bigint(13) NOT NULL,
-  `medic` binary(1) NOT NULL DEFAULT '\0',
-  `super_user` binary(1) NOT NULL DEFAULT '\0',
+  `n_secu` int(255) NOT NULL,
+  `medic` binary(1) NOT NULL,
+  `super_user` binary(1) NOT NULL,
   `first_name` varchar(63) NOT NULL,
   `last_name` varchar(63) NOT NULL,
   `birth_date` date NOT NULL,
   `e_mail` varchar(63) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `last_connect` date DEFAULT NULL,
+  `password` varchar(63) NOT NULL,
+  `last_connect` date NOT NULL,
   `nb_try` int(16) NOT NULL DEFAULT '0',
-  `n_secu_medic` bigint(13) DEFAULT NULL,
+  `n_secu_medic` int(255) DEFAULT NULL,
   `id_hospital` int(32) DEFAULT NULL,
   PRIMARY KEY (`n_secu`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`n_secu`, `medic`, `super_user`, `first_name`, `last_name`, `birth_date`, `e_mail`, `password`, `last_connect`, `nb_try`, `n_secu_medic`, `id_hospital`) VALUES
-(269054958815780, 0x00, 0x00, 'Thomas', 'Hercule', '1999-12-08', 'thomas.hercule@isep.fr', '$2y$10$z6Cysx1ncOxi14ij13m5Cepbnsy1ELjTC.QY.EE7KapuRhZzw5ziW', NULL, 0, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
