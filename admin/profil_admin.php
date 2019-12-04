c6f8aba5f4b28fd021a6d4b9a68c966499f0d878
maximeguasp <55990281+maximeguasp@users.noreply.github.com>
2019-12-04 11:14:27 +0100

    page de test (php + CSS)

---
+++ admin/profil_admin.php
@@ -0,0 +1,145 @@
+
+<!DOCTYPE html>
+
+
+<html>
+
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="../CSS/style.css" />
+	<link rel="stylesheet" href="../CSS/style-profil_admin.css" />
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'> <!--Permet de telecharger la police Source sans pro -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous"> <!-- Permet d'utiliser les émoticions de fontawesome -->
+	<title>Audasanté</title>
+</head>
+
+
+<body>
+	
+
+	<header><h1>AUDASANTÉ</h1></header>
+
+	<?php
+		require('menu_lateral_admin.php');
+		require('top_bar.php')
+	?>
+		        
+		        <h2> Profil Administtrateur </h2>
+
+
+		        	<div id="main-section">
+		        		<div class="patient">
+		        			<p>Test    Patient N°</p>
+		        		 </div>
+		        		<div class ="menu"> 
+
+
+		        			
+		        			<div class="interfaces"> 
+
+		        				<div class=" info-prog">
+
+
+
+		        					<div class="info"> <h3> Informations</h3>
+		        					</br><h4>
+		        						Test actuel:</br>
+		        						N°patient:</br>
+		        						Nom:</br>
+		        						Ref HIG:</br>
+		        						Date de naissance:</br>
+		        						Commentaire:</br>
+</h4>
+		        					</div>
+		        					<div class="prog"><h3> Progression</h3>
+		        					</div>
+
+		        				</div>
+
+		        				<div class=" test"> 
+
+		        					<div class="prompteur"><h3> Prompteur patient<h3>
+                                       <img src="listener.png" alt="listener">
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    </br>
+                                    
+                                    
+                                
+
+                                   <h10> Appuyez sur l'interrupteur
+                                    lorsque vous entendez un son</h10>
+		        					</div>
+		        					<div class="decompte"> <h5>
+                                        Décompte</br></br>
+                                        X</br>
+                                       <h6>SECONDES RESTANTES<h6>
+
+                                           <h7> Tenez vous prêt pour</br>
+                                            le départ du test; <h7>
+                                           
+
+
+		        					</div>
+
+		        				</div>
+		        				
+
+		        				<div class="etape">
+
+                                <div id="moncercle1">
+                                    <h9> 1</h9>
+                                </br>
+                                </br>
+                                  <h8>  Rythme cardiaque<h8>
+                                </div>
+                                <hr>
+                                <div id="moncercle2">
+                                    <h9>2</h9>
+                                </br>
+                                </br>
+                                  <h8>  Température corporelle</h8>
+                                </div>
+                                <hr>
+                                <div id="moncercle3">
+                                    <h9>3</h9>
+                                </br>  
+                                </br>
+                                <h8>Réaction auditive</h8>
+                                </div>
+                                <hr>
+                                <div id="moncercle4">
+                                    <h9>4</h9>
+                                </br>
+                                </br>
+                                  <h8>  Test d'audition</h8>
+                                </div>
+
+		        				</div>
+
+
+		        			</div>
+		        			<div class="API"> 
+		        				API
+		        			</div>
+		        		</div>		
+		        	</div>
+
+
+
+
+
+<body>
