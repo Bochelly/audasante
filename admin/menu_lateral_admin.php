6ccce84d6d9c6abd19b063ddf5b34d119b15c967
Felixned <55989975+Felixned@users.noreply.github.com>
2019-12-06 01:31:02 +0100

    menu latéral responsive (téléphone tout du moins)

--- admin/menu_lateral_admin.php
+++ admin/menu_lateral_admin.php
@@ -1,6 +1,7 @@
 
-<nav> 
+<nav id="menu"> 
 		<ul>
+			<div id='croix'><a href=''><i class="far fa-times-circle fa-lg fa-fw"></i></a></div>
 		    <a href="index"><li <?php if ($page_en_cours == 'accueil') {echo 'class="en_cours"';} ?>><i class="fas fa-home fa-lg fa-fw"><!--Icon--></i><span>Accueil</span></li></a>
 		    <a href="dashboard"><li <?php if ($page_en_cours == 'dashboard') {echo 'class="en_cours"';} ?>><i class="far fa-chart-bar fa-lg fa-fw"> </i><span>Dashboard</span></li></a>
 		    <a href="messagerie"><li <?php if ($page_en_cours == 'messagerie') {echo 'class="en_cours"';} ?>><i class="fas fa-envelope fa-lg fa-fw"> </i><span>Messages</span></li></a>	
@@ -11,4 +12,4 @@
 		</ul>
 </nav>
 
-<div class='burger'><a href='#'><i class="fas fa-grip-lines fa-lg fa-4x"></i></a></div>
+<div class='burger'><a href="#menu"><i class="fas fa-grip-lines fa-lg fa-3x"></i></a></div>
