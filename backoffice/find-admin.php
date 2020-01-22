b854c684c862769260f401debda42fb32c443cf6
bochelly <antoine.borrelly@isep.fr>
2020-01-22 12:11:17 +0100

    Ajout de la recherche pour admin + fonction d'éditions d'utilisateurs dans leur tableau

---
+++ backoffice/find-admin.php
@@ -0,0 +1,169 @@
+<?php $page_en_cours = 'none';
+session_start();
+require('securiteBackoffice.php');
+require('../config.php');
+?>
+
+<!DOCTYPE html>
+
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="../CSS/style.css" />
+		<link rel="stylesheet" href="../CSS/style-users-admin.css"/>
+		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'> <!--Permet de telecharger la police Source sans pro -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous"> <!-- Permet d'utiliser les émoticions de fontawesome -->
+		<title>Audasanté - Résultats de la recherche</title>
+	</head>
+	<body>
+		<header><h1>AUDASANTÉ</h1></header>
+
+		<?php
+
+            $search_text = "";
+            $full_name = array();
+			require('menu_lateral_backoffice.php');
+			require('top_bar.php');
+			//$requete_1 = $bdd->prepare("SELECT first_name ,last_name ,n_secu FROM user WHERE medic=FALSE");
+			//$sql->setFetchMode(PDO::FETCH_ASSOC);
+		?>
+
+        <?php
+
+        //on vérifie qu'on ai bien toute les informatoins d'inscriptions
+        if(empty($_POST['search-text']) || $_POST['search-text']=="")
+        {
+            echo("<div id=\"contenuAssistance\"><h2>La recherche n'a renvoyé aucun résultat ! Vérifiez votre saisie ou inscrivez l'utilisateur.</h2></div>");
+            //$error_type = "nolog";
+            //header('Location: index.php?error_type='.$error_type.'');
+            //exit();
+        }
+        else
+        {
+            $search_text = $_POST['search-text'];
+            $full_name = explode(" ", $search_text); //ainsi full_name[1] prénom et l'autre nom
+            echo ($full_name[0]);
+            ?>
+            <div id="contenuAssistance">
+                <h2>Résultats de la recherche</h2>
+
+                <div class="form-popup" id="edit-form">
+
+                    <form action="user_edit.php" class="form-container" method="post">
+                        <h3>Edit user</h3>
+
+                        <?php
+                        $reponse = $bdd->query('SELECT * FROM user');
+                        ?>
+                        <h4 id="name-title">Name</h4>
+
+                        <label for="first_name"><b>First name</b></label>
+                        <input id="surname-input" type="text" placeholder="Enter First Name" name="first_name" value="Name" required>
+
+                        <label for="text"><b>Last name</b></label>
+                        <input id="name-input" type="text" placeholder="Enter Last Name" name="last_name" value="Name" required>
+
+
+
+                        <label for="number"><b>N° Sécu</b></label>
+                        <input id="no-input" type="number" placeholder="0000000000" name="n_secu" value=00000000 readonly="readonly" required>
+
+
+                        <label for="mail"><b>Email</b></label>
+                        <input id="email-input" type="text" placeholder="Enter Email" name="e_mail" value="Email" required>
+
+
+                        <button type="submit" class="btn save">Save</button>
+                        <button type="button" class="btn cancel" onclick="closeForm()">Cancel</button>
+                    </form>
+                </div>
+
+                <div class="delete-user-form" id="delete-section">
+
+                    <form id="delete-form" action="user_delete.php" method="post">
+                        <label for="number"><b>N° Sécu</b></label>
+                        <input id="user-to-delete"name="n_secu" value="0" required>
+                        <button type="submit" class="btn delete">Delete</button>
+                    </form>
+                </div>
+
+
+                <div id="mail">
+                    <h3>Utilisateurs trouvé(s) pour : "<?php echo ($full_name[0].' '.$full_name[1])?>"</h3>
+
+                    <!-- Inserer le tableau ici -->
+                    <table>
+
+                        <tr>
+                            <th>NOM</th>
+                            <th>N° SÉCU</th>
+                            <th>EMAIL</th>
+                            <th>DERNIÈRE CO.</th>
+                            <th>ACTIONS</th>
+                        </tr>
+
+                        <?php
+                        //echo ($full_name[0] . $full_name[1]);
+                        $pquery = "SELECT * FROM user WHERE first_name ='".$full_name[0]."' AND last_name ='".$full_name[1]."'";
+                        $reponse = $bdd->query($pquery);
+                        while ($data = $reponse->fetch()) {
+                            ?>
+                            <tr>
+                                <td><?php echo $data['first_name']; echo " "; echo $data['last_name']; ?></td>
+                                <td><?php echo $data['n_secu']; ?></td>
+                                <td><?php echo $data['e_mail']; ?></td>
+                                <td><?php echo $data['last_connect']; ?></td>
+                                <td><span class="delete" onclick="confirmMessage('<?php echo $data['n_secu'] ?>')">Delete</span> <span class="edit" onclick="openForm('<?php echo $data['first_name'] ?>','<?php echo $data['last_name'] ?>','<?php echo $data['n_secu'] ?>','<?php echo $data['e_mail'] ?>')">Edit</span></td>
+
+                            </tr>
+                        <?php } $reponse->closeCursor(); ?>
+                        <?php
+                        //POUR L'INVERSE NOM - PRENOM
+                        $pquery = "SELECT * FROM user WHERE n_secu ='".$full_name[0]."'";
+                        $reponse = $bdd->query($pquery);
+                        while ($data = $reponse->fetch()) {
+                            ?>
+                            <tr>
+                                <td><?php echo $data['first_name']; echo " "; echo $data['last_name']; ?></td>
+                                <td><?php echo $data['n_secu']; ?></td>
+                                <td><?php echo $data['e_mail']; ?></td>
+                                <td><?php echo $data['last_connect']; ?></td>
+                                <td><span class="delete" onclick="confirmMessage('<?php echo $data['n_secu'] ?>')">Delete</span> <span class="edit" onclick="openForm('<?php echo $data['first_name'] ?>','<?php echo $data['last_name'] ?>','<?php echo $data['n_secu'] ?>','<?php echo $data['e_mail'] ?>')">Edit</span></td>
+
+                            </tr>
+                        <?php } $reponse->closeCursor(); ?>
+                        <?php
+                        $pquery = "SELECT * FROM user WHERE first_name ='".$full_name[1]."' AND last_name ='".$full_name[0]."'";
+                        $reponse = $bdd->query($pquery);
+                        while ($data = $reponse->fetch()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $data['first_name']; echo " "; echo $data['last_name']; ?></td>
+                            <td><?php echo $data['n_secu']; ?></td>
+                            <td><?php echo $data['e_mail']; ?></td>
+                            <td><?php echo $data['last_connect']; ?></td>
+                            <td><span class="delete" onclick="confirmMessage('<?php echo $data['n_secu'] ?>')">Delete</span> <span class="edit" onclick="openForm('<?php echo $data['first_name'] ?>','<?php echo $data['last_name'] ?>','<?php echo $data['n_secu'] ?>','<?php echo $data['e_mail'] ?>')">Edit</span></td>
+
+                        </tr>
+                        <?php } $reponse->closeCursor(); ?>
+
+                    </table>
+
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+
+
+        <?php //echo("<div id=\"contenuAssistance\"><h2>La recherche n'a renvoyé aucun résultat ! Vérifiez votre saisie ou inscrivez l'utilisateur.</h2></div>");
+        //$error_type = "nolog";
+        //header('Location: index.php?error_type='.$error_type.'');
+        //exit();?>
+
+
+
+		<script src="../js/script.js"></script>
+	</body>
+</html>
--- backoffice/top_bar.php
+++ backoffice/top_bar.php
@@ -1,42 +1,43 @@
-<?php 	require('../config.php');	
-		$profil = $bdd->query('SELECT * FROM user WHERE n_secu='.$_SESSION["n_secu"].'');
-		$data = $profil->fetch();
+<?php 	require('../config.php');
+$profil = $bdd->query('SELECT * FROM user WHERE n_secu='.$_SESSION["n_secu"].'');
+$data = $profil->fetch();
 ?>
 
 <div id='top-bar'>
-	<ul>
-		<form autocomplete="off"  >
-			<div class="autocomplete">
-				<li id="search-field"><i class="fas fa-search" style="color:#A5A4BF;"></i>
-					<input id="search-text" type="text" name="search-bar" placeholder="Recherchez un patient, un test ou un message">
-					<input type="submit" hidden>
-				</li>
-			</div>
-		</form>
-		<li id="top-photo"><a href="#menu_profil"> <div id='nom'><?php echo $data['first_name'].' '.$data['last_name']; ?></div>		
+    <ul>
+        <form autocomplete="off" method="POST" action="find-admin.php" >
+            <div class="autocomplete">
+                <li id="search-field"><i class="fas fa-search" style="color:#A5A4BF;"></i>
+                    <input id="search-text" type="text" name="search-text" placeholder="Recherchez un patient, un test ou un message">
+                    <input type="submit" hidden>
+                </li>
+            </div>
+        </form>
+        <li id="top-photo"><a href="#menu_profil"> <div id='nom'><?php echo $data['first_name'].' '.$data['last_name']; ?></div>
 
-			<?php 
-				if(!empty($data['photo']))
-				{
-			?>
-				<img id='photo_profil' src="../images/images_profil/<?php echo $data['photo']; ?>">
-			<?php		
-				}
-			?>
-
-
-			</a>
+                <?php
+                if(!empty($data['photo']))
+                {
+                    ?>
+                    <img id='photo_profil' src="../images/images_profil/<?php echo $data['photo']; ?>">
+                    <?php
+                }
+                ?>
 
 
-
-			 <div id="menu_profil">
-					<ul>
-						<a class='noTransition' href="profil.php"><li> Profil  </li></a>
-						<a class='noTransition' href="disconnect.php"><li> Déconnexion  </li></a>
-					</ul>
-			</div>
-		</li>
+            </a>
 
 
-	</ul>
+
+            <div id="menu_profil">
+                <ul>
+                    <a class='noTransition' href="profil.php"><li> Profil  </li></a>
+                    <a class='noTransition' href="disconnect.php"><li> Déconnexion  </li></a>
+                </ul>
+            </div>
+        </li>
+        <li id="top-assist"> <a href="assistance"><i class="far fa-question-circle fa-lg fa-fw" style="color:#A5A4BF;" title="assistance"> </i></a> </li>
+        <li id="top-messagerie"><a href="messagerie"><i class="fas fa-envelope fa-lg fa-fw" style="color:#A5A4BF;" title="messagerie"> </i></a> </li>
+
+    </ul>
 </div>
